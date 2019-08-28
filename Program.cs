26d287c18209d20d3de832322b112c1a793aeb42
Sergej Schmal <s.shmall@hotmail.com>
2019-08-28 20:01:12 +0200

    Add logic for check of links

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace CheckLinksConsole
 {
@@ -6,8 +12,30 @@ namespace CheckLinksConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            var site = "https://g0t4.github.io/pluralsight-dotnet-core-xplat-apps";
+            var client = new HttpClient();
+            var body = client.GetStringAsync(site);
+
+            Console.WriteLine(body.Result);
+
+            Console.WriteLine();
+            Console.WriteLine("Links:");
+            var links = LinkChecker.GetLinks(body.Result);
+            links.ToList().ForEach(Console.WriteLine);
+        }
+    }
+
+    internal class LinkChecker
+    {
+        public static IEnumerable<string> GetLinks(string page)
+        {
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(page);
+            var links = htmlDocument.DocumentNode.SelectNodes("//a[@href]")
+                .Select(n => n.GetAttributeValue("href", string.Empty))
+                .Where(l => !string.IsNullOrEmpty(l))
+                .Where(l => l.StartsWith("http"));
+            return links;
         }
     }
 }
