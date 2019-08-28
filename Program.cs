e798e349678fb996537e7ee703a2012de172f8f1
Sergej Schmal <s.shmall@hotmail.com>
2019-08-28 19:13:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CheckLinksConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
